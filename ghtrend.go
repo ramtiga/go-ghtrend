5492234f0d05e984ce21e7ee604ca6aba12ae3aa
ramtiga <dhanegm731@gmail.com>
2014-03-19 23:58:48 +0900

    Added comannd option -b and -v

--- ghtrend.go
+++ ghtrend.go
@@ -5,15 +5,18 @@ import (
         "fmt"
         "os"
         "github.com/PuerkitoBio/goquery"
+        "os/exec"
 )
 
 type RepoInf struct {
         RepositoryName string
         Description    string
+        RepoUrl        string
 }
 
 const (
         TREND_MAX_NUM = 25
+        VERSION = "0.0.1"
 )
 
 var repoInf []RepoInf
@@ -21,10 +24,12 @@ var baseUrl string = "https://github.com/trending"
 
 
 var (
-        lang = flag.String("l", "all", "Select language")
-        desc = flag.Bool("d", false, "Show description")
-        num  = flag.Int("n", 10, "Limit numbers")
-        help = flag.Bool("h", false, "Show help message")
+        lang    = flag.String("l", "all", "Select language")
+        desc    = flag.Bool("d", false, "Show description")
+        num     = flag.Int("n", 10, "Limit numbers")
+        brows   = flag.Int("b", 0, "Show repository on browser")
+        help    = flag.Bool("h", false, "Show help message")
+        version = flag.Bool("v", false, "Show version")
 )
 
 func main() {
@@ -36,26 +41,38 @@ optional arguments:
   -l    Select language.
   -d    Show description.
   -n    Limit numbers.
+  -b    Show repository on browser.
   -h    Show help message.
+  -v    Show version.
 `)
         }
         flag.Parse()
 
+        if *version {
+                showVersion()
+                os.Exit(0)
+        }
         if *help {
                 flag.Usage()
                 os.Exit(0)
         }
+        
         n := getNum(*num)
-        url := getUrl(*lang)
+        url := getGithubUrl(*lang)
 
         repoInf = getMemory(n)
 
         getPage(url, n)
 
+        if *brows > 0 && *brows <= 25 {
+                browsUrl := getBrowsUrl(*brows)
+                openBrowser(browsUrl)
+                os.Exit(0)
+        }
         showResult()
 }
 
-func getUrl(lang string) string {
+func getGithubUrl(lang string) string {
         if lang == "" {
                 return baseUrl
         } else {
@@ -77,13 +94,10 @@ func getMemory(num int) []RepoInf {
 func getPage(url string, num int) {
         doc, _ := goquery.NewDocument(url)
         doc.Find(".leaderboard-list-content").Each(func(i int, s *goquery.Selection) {
-                // fmt.Println(s.Find(".owner-name").Text())
-                // fmt.Println(s.Find("span[class='owner-name']").Text())
-                // fmt.Println(s.Find("strong").Text())
                 if i < num {
-                        // fmt.Println(s.Find("a[class='repository-name']").Text())
                         repoInf[i].RepositoryName = s.Find("a[class='repository-name']").Text()
                         repoInf[i].Description = s.Find("p[class='repo-leaderboard-description']").Text()
+                        repoInf[i].RepoUrl = s.Find("a[class='repository-name']").Text()
                 }
 
         })
@@ -110,4 +124,16 @@ func showResult() {
                         fmt.Println(spaces + rp.Description)
                 }
         }
+}
+
+func getBrowsUrl(idx int) string {
+        return "https://github.com/" + repoInf[idx - 1].RepoUrl
+}
+
+func openBrowser(url string) {
+        exec.Command("open", url).Run()
+}
+
+func showVersion() {
+        fmt.Printf("ghtrend Ver %s\n", VERSION)
 }
